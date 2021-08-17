b8aa5f33ed31b4ebafe64a6ffcb37bdf46893ecc
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2021-08-18 01:34:42 +0200

    Add project files.

---
+++ ShList.BlazorSrv/Pages/Products.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShList.Dto;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ShList.BlazorSrv.Pages
+{
+    public partial class Products
+    {
+        private IEnumerable<ProductDto> _productDtos = new List<ProductDto>();
+
+        [Inject]
+        private HttpClient _httpClient { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            _productDtos = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Products");
+        }
+    }
+}
