8652153fc83aa4de0829157fb46e807503b40c75
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2021-08-20 01:24:05 +0200

    First working submit

--- ShList.BlazorSrv/Pages/ProductEdit.razor.cs
+++ ShList.BlazorSrv/Pages/ProductEdit.razor.cs
@@ -15,26 +15,48 @@ namespace ShList.BlazorSrv.Pages
         [Inject]
         private IProductService _productService { get; set; }
 
+
+
+        //For UI
+        protected string Message { get; set; }
+        protected string StatusClass { get; set; } = "alert-success";
+        protected bool Saved { get; set; } = true;
+
+
         private Product _product { get; set; }
 
-        public string Name { get; set; }
-        public string Notes { get; set; }
+        //public string Name { get; set; }
+        //public string Notes { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             
-            if (!string.IsNullOrEmpty(ProductId))
+            if (string.IsNullOrEmpty(ProductId))
             {
-                _product = await _productService.Get(ProductId);
+                _product = new Product();
+                Saved = true;
             }
             else
             {
-                _product = new Product();
+                _product = await _productService.Get(ProductId);
+                Saved = false;
             }
-
-
-
             await base.OnInitializedAsync();
         }
+
+        protected async Task HandleValidSubmit()
+        {
+            _product = await _productService.AddOrUpdate(_product);
+            Saved = true;
+            Message = "Saved!";
+        }
+
+        protected async Task HandleInvalidSubmit()
+        {
+            StatusClass = "alert-danger";
+            Message = "Invalid Data";
+        //_product = await _productService.AddOrUpdate(_product);
+    }
+
     }
 }
--- ShList.BlazorSrv/Services/Models/ProductService.cs
+++ ShList.BlazorSrv/Services/Models/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using ShList.BlazorSrv.Models;
+using ShList.BlazorSrv.Pages;
 using ShList.BlazorSrv.Services.Interfaces;
 using ShList.Dto;
 using System.Collections.Generic;
@@ -11,7 +12,6 @@ namespace ShList.BlazorSrv.Services.Models
 {
     public class ProductService : IProductService
     {
-
         private HttpClient _httpClient { get; set; }
 
         public ProductService(HttpClient httpClient)
@@ -22,13 +22,23 @@ namespace ShList.BlazorSrv.Services.Models
         public async Task<IEnumerable<Product>> Get()
         {
             var dtos = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("/api/Products");
+            //TODO Check the result and act accordingly
             return dtos.Select(dto => new Product(dto));
         }
 
         public async Task<Product> Get(string productId)
         {
             ProductDto dto = await _httpClient.GetFromJsonAsync<ProductDto>($"/api/Products/{productId}");
+            //TODO Check the result and act accordingly
             return new Product(dto);
+        }
+
+        public async Task<Product> AddOrUpdate(Product product)
+        {
+            ProductDto dto = new ProductDto(product.Id, product.Name, product.Notes);
+            //TODO Check the result and act accordingly.
+            await _httpClient.PostAsJsonAsync<ProductDto>($"/api/Products",dto);
+            return product; //Quick and dirty because I know product is not changed, but the right way is to deserialize the outcome of a successful post
         }
     }
 }
